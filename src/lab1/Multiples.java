395cbc88cb57105b27f1a26d4c1520f909bbe1f1
peterlee42 <76502486+peterlee42@users.noreply.github.com>
2025-09-09 12:44:55 -0400

    finished lab

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,36 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples();
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int total = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 && i % b == 0) {
+                total += 1;
+            } else if (i % a == 0) {
+                total += 1;
+            } else if (i % b == 0) {
+                total += 1;
+            }
+        }
+        return total;
+    }
+
+    public static int multiples() {
+        int total = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                total += 1;
+            } else if (i % 3 == 0) {
+                total += 1;
+            } else if (i % 5 == 0) {
+                total += 1;
+            }
+        }
+        return total;
+    }
+}
